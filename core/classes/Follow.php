700d7ebddec1f6e09ee7381813ae5ca3fa853b58
chrisqwesiglikpo <danielqwesiotu@gmail.com>
2020-08-07 07:10:16 +0100

    Follow section done

--- core/classes/Follow.php
+++ core/classes/Follow.php
@@ -5,6 +5,35 @@ class Follow extends User {
 	function __construct($pdo){
      $this->pdo=$pdo;
 	}
+
+	public function checkFollow($followerID,$user_id){
+		$stmt=$this->pdo->prepare("SELECT * FROM `follow` WHERE `sender` =:user_id AND `receiver` =:followerID");
+		$stmt->bindParam(":user_id",$user_id,PDO::PARAM_INT);
+		$stmt->bindParam(":followerID",$followerID,PDO::PARAM_INT);
+		$stmt->execute();
+		return $stmt->fetch(PDO::FETCH_ASSOC);
+	}
+
+	public function followBtn($profileID,$user_id){
+		$data=$this->checkFollow($profileID,$user_id);
+		if($this->loggedIn()===true){
+			if($profileID != $user_id){
+                if(!empty($data['receiver'])==$profileID){
+                     //Following btn
+                	echo "<button class='f-btn following-btn follow-btn' data-follow='".$profileID."'>Following</button>";
+                }else{
+                    //follow button
+                    echo "<button class='f-btn following-btn follow-btn' data-follow='".$profileID."'><i class='fa fa-user-plus'></i>Follow</button>";
+                }
+			}else{
+				//edit button
+				echo "<button class='f-btn' onclick=location.href='profileEdit.php'>Profile Edit</button>";
+			}
+
+		}else{
+			echo "<button class='f-btn' onclick=location.href='index.php'><i class='fa fa-user-plus'></i>Follow</button>";
+		}
+	}
 	
 }
 
--- profile.php
+++ profile.php
@@ -124,7 +124,7 @@
 	</ul>
 	<div class="edit-button">
 		<span>
-			<button class="f-btn follow-btn"  data-follow="user_id" data-user="user_id"><i class="fa fa-user-plus"></i> Follow </button>
+			<?php $getFromF->followBtn($profileId,$user_id); ?>
 		</span>
 	</div>
     </div>
